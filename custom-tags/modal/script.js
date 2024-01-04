39e16d55a02a54dfa9e7d1152398e537ce74da98
stravo1@mac <stravoone@gmail.com>
2024-01-04 22:17:19 +0530

    fix: added default content to modal

--- custom-tags/modal/script.js
+++ custom-tags/modal/script.js
@@ -4,14 +4,28 @@ const modalBottom = document.querySelector(".modal-bottom");
 const modalLink = document.querySelector(".modal-link");
 const modalHeader = document.querySelector(".modal-header");
 const modalContent = document.querySelector(".modal-content");
-
+const holderDefaultContent = `
+Sample <b>Content</b>...
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat atque voluptate
+quisquam rem. Nulla non debitis fugit suscipit beatae consectetur recusandae
+vitae laborum libero unde voluptatibus cum, numquam quis ill
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat atque voluptate
+quisquam rem. Nulla non debitis fugit suscipit beatae consectetur recusandae
+vitae laborum libero unde voluptatibus cum, numquam quis ill
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat atque voluptate
+quisquam rem. Nulla non debitis fugit suscipit beatae consectetur recusandae
+vitae laborum libero unde voluptatibus cum, numquam quis ill
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat atque voluptate
+quisquam rem. Nulla non debitis fugit suscipit beatae consectetur recusandae
+vitae laborum libero unde voluptatibus cum, numquam quis illum!
+`
 function openModal(headerName, description, link) {
     modalWrapper.style.display = "flex";
     setTimeout(() => {
         modalWrapper.style.opacity = 1;
     }, 1)
-    if (headerName) modalHeader.innerHTML = headerName;
-    if (description) modalContent.innerHTML = description;
+    if (headerName) { modalHeader.innerHTML = headerName; } else { modalHeader.innerHTML = "Sample Header" }
+    if (description) { modalContent.innerHTML = description } else { modalContent.innerHTML = holderDefaultContent };
     if (!link) {
         modalLink.style.display = "none";
         modalBottom.style.backgroundColor = "white";
